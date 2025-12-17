35c72325c5f19499504c0c10161e2a24f8c5ecac
tajjacob <63710008+tajjacob@users.noreply.github.com>
2025-12-17 14:38:56 +0800

    ⚗️ tutorial: 64. CORS
    ✨ feat: add CORS configuration for development and production environments; update application URLs in launchSettingsd

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+// command to run the application with HTTPS profile
+// dotnet run --launch-profile https
+
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,22 +13,46 @@ builder.Services.AddControllers(); // explanation: register controller services 
 builder.Services.AddEndpointsApiExplorer(); // explanation: use to explore API endpoints for Swagger
 builder.Services.AddSwaggerGen(); // explanation: use to generate Swagger documentation
 
+builder.Services.AddCors((options) => // explanation: configure CORS policies. CORS means Cross-Origin Resource Sharing. it is a security feature implemented by browsers to restrict web applications running on one origin (domain) from accessing resources on a different origin.
+    {
+        options.AddPolicy("DevCors", (corsBuilder) => // explanation: define a CORS policy named "DevCors" for development environment
+            {
+                corsBuilder.WithOrigins("http://localhost:4200", "http://localhost:3000", "http://localhost:8000") // explanation: specify the allowed origins (domains) that can access the API during development
+                    .AllowAnyMethod() // explanation: allow any HTTP method (GET, POST, PUT, DELETE, etc.) from the specified origins 
+                    .AllowAnyHeader() // explanation: allow any HTTP headers from the specified origins
+                    .AllowCredentials(); // explanation: allow cookies and authentication information to be sent with requests from the specified origins
+            });
+        options.AddPolicy("ProdCors", (corsBuilder) =>
+            {
+                corsBuilder.WithOrigins("https://myProductionSite.com")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            });
+    });
+ 
 var app = builder.Build();
-
+ 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    // app.MapOpenApi(); // explanation: use to serve OpenAPI/Swagger UI in development environment
-    app.UseSwagger(); // explanation: enables middleware to serve generated Swagger as a JSON endpoint
-    app.UseSwaggerUI(); // explanation: enables middleware to serve swagger-ui (HTML, JS, CSS, etc.)
+    app.UseCors("DevCors"); // explanation: apply the "DevCors" CORS policy in the development environment
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseHttpsRedirection();
 }
 else
 {
-app.UseHttpsRedirection();
+    app.UseCors("ProdCors");
     app.UseHttpsRedirection();
 }
 
 app.MapControllers(); // explanation: map controller routes to the request pipeline
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapGet("/", () => Results.Redirect("/swagger/index.html"));
+}
 
 // app.MapGet("/weatherforecast", () =>
 // {
@@ -33,5 +60,3 @@ app.MapControllers(); // explanation: map controller routes to the request pipel
 // .WithName("GetWeatherForecast");
 
 app.Run(); // explanation: starts the web application and listens for incoming HTTP requests
-
-
