879bd619efa5a7c6a372b7a26161c12770ea8fa6
tajjacob <63710008+tajjacob@users.noreply.github.com>
2026-01-12 09:32:27 +0800

    ⚗️ tutorial: 83. UserRepositoryd
    - Implement UserRepository class for user data management

---
+++ Data/UserRepository.cs
@@ -0,0 +1,32 @@
+namespace DotnetAPI.Data
+{
+    public class UserRepository
+    {
+         DataContextEF _entityFramework;   
+        public UserEFController(IConfiguration config )
+        {
+            _entityFramework = new DataContextEF(config);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_entityFramework.SaveChanges() >= 0);
+        }
+
+        public void AddEntity<T>(T entityToAdd)
+        {   
+            if (entityToAdd != null)
+            {
+                _entityFramework.Add(entityToAdd);
+            }
+        } 
+
+        public void RemoveEntity<T>(T entityToRemove)
+        {   
+            if (entityToRemove != null)
+            {
+                _entityFramework.Remove(entityToRemove);
+            }
+        }
+    }
+}
