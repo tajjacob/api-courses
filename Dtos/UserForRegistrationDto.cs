4ce641141a2ecc42b27051e7466844bc91b173fb
tajjacob <63710008+tajjacob@users.noreply.github.com>
2026-01-19 15:42:41 +0800

    ⚗️ tutorial: 87. Auth Settings and Models
    - Add DTOs for user login and registration, and update appsettings

---
+++ Dtos/UserForRegistrationDto.cs
@@ -0,0 +1,12 @@
+namespace DotnetAPI.Dtos
+
+{
+  public partial class UserForRegistrationDto
+  {
+    public string Email { get; set; } = "";
+    public string Password { get; set; } = "";
+    public string PasswordConfirm { get; set; } = "";
+  }
+ 
+    
+}
