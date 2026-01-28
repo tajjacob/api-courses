3c979362076172213bbbc0091a356cf316bf2bf0
tajjacob <63710008+tajjacob@users.noreply.github.com>
2026-01-28 11:25:21 +0800

    ⚗️ tutorial: 90. Registration With Details
    - Implement user registration and add user details to the database

--- Dtos/UserForRegistrationDto.cs
+++ Dtos/UserForRegistrationDto.cs
@@ -6,6 +6,9 @@ namespace DotnetAPI.Dtos
     public string Email { get; set; } = "";
     public string Password { get; set; } = "";
     public string PasswordConfirm { get; set; } = "";
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public string Gender { get; set; } = "";
   }
  
     
