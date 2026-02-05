2e23bd42b680dbee7657c079c780f5f4f4c871fa
tajjacob <63710008+tajjacob@users.noreply.github.com>
2026-02-05 17:03:11 +0800

    ⚗️ tutorial: 99. Parameters
    - Update spUsers_Get procedure and add notes on stored procedures

--- SQLQuery_section09.sql
+++ SQLQuery_section09.sql
@@ -3,7 +3,8 @@ GO
 
 
 ALTER PROCEDURE TutorialAppSchema.spUsers_Get 
--- EXEC TutorialAppSchema.spUsers_Get
+-- EXEC TutorialAppSchema.spUsers_Get @UserId = 3
+@UserId INT 
 AS 
 BEGIN
     SELECT [Users].[UserId],
@@ -13,6 +14,7 @@ BEGIN
             [Users].[Gender],
             [Users].[Active] 
     FROM TutorialAppSchema.Users AS Users 
+    WHERE [Users].[UserId] = @UserId
 END
 
 
