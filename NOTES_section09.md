eb541b2b62e291e49fa0bf443457958157888764
tajjacob <63710008+tajjacob@users.noreply.github.com>
2026-02-09 11:27:48 +0800

    ⚗️ tutorial: 101. Enhance spUsers_Get procedure with additional salary and job title information

--- NOTES_section09.md
+++ NOTES_section09.md
@@ -13,3 +13,17 @@
 3. **Maintainability**: If your database schema changes (e.g., a column name changes), you only need to update the Stored Procedure in the database, rather than finding and updating every SQL string scattered throughout your C# code.
 4. **Network Traffic**: Instead of sending a long SQL query string over the network, you only send the name of the procedure and parameters.
 
+# What is a Temp Table?
+
+- a table that exists temporarily on the database server. They are stored in the system database *tempdb* and are useful for storing intermediate results during the execution of a complex query or stored procedure.
+
+## There are two main types:
+
+1. **Local Temp Tables (#TableName):** Visible only to the connection that created them. They are automatically deleted when the connection closes.
+2. **Global Temp Tables (##TableName):** Visible to all connections. They are deleted when the last connection referencing them is closed.
+
+## Using a temp table here is an optimization strategy
+
+- **Performance:** Instead of calculating the average salary for the department inside a subquery for every single user (which can be computationally expensive), you calculate it once for all departments, store it, index it, and then join it.
+- **Readability:** It breaks a complex logic flow into distinct steps: "First, figure out department averages. Second, get user data and attach those averages."
+
--- SQLQuery_section09.sql
+++ SQLQuery_section09.sql
@@ -4,17 +4,30 @@ GO
 
 ALTER PROCEDURE TutorialAppSchema.spUsers_Get 
 -- EXEC TutorialAppSchema.spUsers_Get @UserId = 3
-@UserId INT = NULL 
+@UserId INT = NULL,
+@Active BIT = NULL 
+
 AS 
 BEGIN
-   /*  SELECT UserJobInfo.Department,
+
+    -- IF OBJECT_ID('tempdb..#TempAvgDeptSalary') IS NOT NULL
+    --     BEGIN
+    --         DROP TABLE #TempAvgDeptSalary
+    --     END    
+
+    DROP TABLE IF EXISTS #TempAvgDeptSalary    
+
+    SELECT UserJobInfo.Department,
     AVG(UserSalary.Salary) AS AvgSalary 
+    INTO #TempAvgDeptSalary
     FROM TutorialAppSchema.Users AS Users 
     LEFT JOIN TutorialAppSchema.UserSalary 
         AS UserSalary ON UserSalary.UserId = Users.UserId
     LEFT JOIN TutorialAppSchema.UserJobInfo 
         AS UserJobInfo ON UserJobInfo.UserId = Users.UserId
-    GROUP BY UserJobInfo.Department     */ 
+    GROUP BY UserJobInfo.Department   
+
+    CREATE CLUSTERED INDEX cix_TempAvgDeptSalary_Department ON #TempAvgDeptSalary(Department) 
 
     SELECT [Users].[UserId],
             [Users].[FirstName],
@@ -32,20 +45,26 @@ BEGIN
     LEFT JOIN TutorialAppSchema.UserSalary 
         AS UserSalary ON UserSalary.UserId = Users.UserId
     LEFT JOIN TutorialAppSchema.UserJobInfo 
-        AS UserJobInfo ON UserJobInfo.UserId = Users.UserId    
-    OUTER APPLY(
-        SELECT UserJobInfo2.Department,
-            AVG(UserSalary2.Salary) AS AvgSalary 
-        FROM TutorialAppSchema.Users AS Users 
-        LEFT JOIN TutorialAppSchema.UserSalary 
-            AS UserSalary2 ON UserSalary2.UserId = Users.UserId
-        LEFT JOIN TutorialAppSchema.UserJobInfo 
-            AS UserJobInfo2 ON UserJobInfo2.UserId = Users.UserId
-        WHERE UserJobInfo2.Department = UserJobInfo.Department
-        GROUP BY UserJobInfo2.Department
+        AS UserJobInfo ON UserJobInfo.UserId = Users.UserId 
+        LEFT JOIN #TempAvgDeptSalary AS AvgSalary ON AvgSalary.Department = UserJobInfo.Department
+   
+    -- OUTER APPLY(
+    --     SELECT UserJobInfo2.Department,
+    --         AVG(UserSalary2.Salary) AS AvgSalary 
+    --     FROM TutorialAppSchema.Users AS Users 
+    --     LEFT JOIN TutorialAppSchema.UserSalary 
+    --         AS UserSalary2 ON UserSalary2.UserId = Users.UserId
+    --     LEFT JOIN TutorialAppSchema.UserJobInfo 
+    --         AS UserJobInfo2 ON UserJobInfo2.UserId = Users.UserId
+    --     WHERE UserJobInfo2.Department = UserJobInfo.Department
+    --     GROUP BY UserJobInfo2.Department
   
-    ) AvgSalary    
+    -- ) AvgSalary    
     WHERE [Users].[UserId] = ISNULL(@UserId, [Users].[UserId])
+    AND [Users].[Active] = ISNULL(@Active, [Users].[Active])
 END
 
+
+-- SELECT CASE WHEN NULL = NULL THEN 1 ELSE 0 END,
+--        CASE WHEN NULL <> NULL THEN 1 ELSE 0 END
 
